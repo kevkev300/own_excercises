44ecf1853720ca95a7fd09d32310bd452f2f2576
Kevin Liebholz <kevin@liebholz.eu>
2020-02-14 18:40:05 +0100

    FEAT: test for interest calc

--- investment_and_interest/calculation.rb
+++ investment_and_interest/calculation.rb
@@ -1,8 +1,10 @@
 class Calculator
-  def calc
-    "hello"
+  def calc_interest(args = {})
+
   end
 
+  # check for a leap-year and returns the daily interest in EUR
+  # daily interest is yearly interest / days in year
   def daily_interest(investment, yearly_interest_rate, year)
     days = ((year - 2020) % 4 != 0 ? 365 : 366)
     ((yearly_interest_rate.to_f / 100) * investment) / days
--- investment_and_interest/spec.rb
+++ investment_and_interest/spec.rb
@@ -1,32 +1,61 @@
 require_relative 'calculation'
-
-
+require 'date'
 
   # Ich investiere zum 8. Februar 2020 den Betrag von 1000 Euro
   # Jeweils am 30. Juni 2020, 2021, 2022 und 2023 soll ich Zinsen iHv 5% p.a. erhalten, sowie eine Rückzahlung iHv 25% meiner Investition
   # Als Zinsberechnungsmethode nehmen wir act/act an
   # Welcher Betrag ist an den vier Stichtagen auszuzahlen?
 
-
-
 RSpec.describe "Calculator" do
   describe '#daily_interest_rate' do
     it 'calculates the right daily interest if leap-year' do
       calculator = Calculator.new
-      interest = calculator.daily_interest_rate(1000, 5, 2020)
-      expect(interest).to be_within(0.0001).of(0.136612)
+      right_interest = ((5.to_f / 100) * 1000) / 366
+
+      interest = calculator.daily_interest(1000, 5, 2020)
+
+      expect(interest).to be_within(0.00001).of(right_interest)
     end
 
     it 'calculates the right daily interest if normal year' do
       calculator = Calculator.new
-      interest = calculator.daily_interest_rate(1000, 5, 2021)
-      expect(interest).to be_within(0.0001).of(0.136986)
+      right_interest = ((5.to_f / 100) * 1000) / 365
+
+      interest = calculator.daily_interest(1000, 5, 2021)
+
+      expect(interest).to be_within(0.00001).of(right_interest)
     end
   end
 
-  describe '#calc' do
-    xit "work with 0 years" do
-      expect(Calculator.new.calc(0, 5, 1000).to eq)
+  describe '#calc_interest' do
+    it "works in leap-year years" do
+      calculator = Calculator.new
+      date_invest = Date.new(2020, 2, 8)
+      date_qualifying = Date.new(2020, 6, 30)
+      right_interest = (((5.to_f / 100) * 1000) / 366) * (date_qualifying - date_invest)
+
+      interest = calculator.calc_interest(years: 0,
+                                          interest: 5,
+                                          investment: 1000,
+                                          date_invest: date_invest,
+                                          date_qualifying: date_qualifying)
+
+      expect(interest).to be_within(0.00001).of(right_interest)
+    end
+
+    it "works in  normal year years" do
+      calculator = Calculator.new
+      date_invest = Date.new(2021, 2, 8)
+      date_qualifying = Date.new(2021, 6, 30)
+      right_interest = (((5.to_f / 100) * 1000) / 365) * (date_qualifying - date_invest)
+
+      interest = calculator.calc_interest(years: 0,
+                                          date_invest: 5,
+                                          investment: 1000,
+                                          date_invest: date_invest,
+                                          date_qualifying: date_qualifying)
+
+      expect(interest).to be_within(0.00001).of(right_interest)
     end
   end
 end
