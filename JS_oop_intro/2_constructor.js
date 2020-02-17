ba3506e55cd4021eec0f97a774135afd178cdd48
Kevin Liebholz <kevin@liebholz.eu>
2020-02-17 11:59:07 +0100

    2_constructors

---
+++ JS_oop_intro/2_constructor.js
@@ -0,0 +1,22 @@
+// Constructor (ES5)
+// !!! arrow function and const don't work
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+
+  this.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
+}
+
+// Instatiante an Object --> run what is in constructor
+// all those things will be typeof == object
+const book1 = new Book('Book One', 'John Doe', '2013');
+const book2 = new Book('Book Two', 'Jane Doe', '2016');
+
+// Access
+console.log(book1.title);
+console.log(book1);
+console.log(typeof book1);
+console.log(book2.getSummary());
